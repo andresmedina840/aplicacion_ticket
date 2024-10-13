a538984c37767de7a3d8178a05daff3ff27c7cce
Carlos Andres Medina Perdomo <andresmedina840@hotmail.com>
2024-10-13 07:35:21 -0500

    Actualizacion

--- src/main/java/co/com/andresmedina/AplicacionTicketApplication.java
+++ src/main/java/co/com/andresmedina/AplicacionTicketApplication.java
@@ -1,5 +1,10 @@
 package co.com.andresmedina;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,15 @@ public class AplicacionTicketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AplicacionTicketApplication.class, args);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy", Locale.of("es", "CO"));
+		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT-5:00"));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("EEEEE dd/MM/yyyy hh:mm:ss a", Locale.of("es", "CO"));
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT-5:00"));
+
+		System.out.println("Funcionando Backend Ticket: " + dateFormat.format(new Date()) + " - " + sdf.format(new Date()));
+
 	}
 
 }
